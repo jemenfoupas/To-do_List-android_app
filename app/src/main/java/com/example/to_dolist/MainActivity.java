c9c1d1e5c0bb933e7671a91303efd8f7c13aaa3d
Rich <boundjirichrich@gmail.com>
2022-08-04 17:12:36 -0600

    made it possible to update a task name and description

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.to_dolist;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-
 
 public class MainActivity extends Activity {
     public static List list = new List();
@@ -29,7 +27,6 @@ public class MainActivity extends Activity {
                 nextPage.putExtra("key", (Integer) btn.getTag());
                 startActivity(nextPage);
             });
-
             ll.addView(btn);
         }
     }
--- app/src/main/java/com/example/to_dolist/addPage.java
+++ app/src/main/java/com/example/to_dolist/addPage.java
@@ -18,21 +18,43 @@ public class addPage extends AppCompatActivity {
         setContentView(R.layout.activity_add_page);
 
         EditText text = findViewById(R.id.taskName);
+        EditText des = findViewById(R.id.editTextTextPersonName2);
         Bundle extras = getIntent().getExtras();
 
         if (extras != null) {
             int value = extras.getInt("key");
             text.setText(MainActivity.list.getName(value));
+            des.setText(MainActivity.list.getDescription(value));
         }
     }
 
     public void addTask(View view) {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) update(view, extras);
+        else addToList(view);
+    }
+
+    public void update(View view, Bundle extras) {
+        EditText name = findViewById(R.id.taskName);
+        EditText des = findViewById(R.id.editTextTextPersonName2);
+        if(name.getText().toString().equals("")){
+            Snackbar notice = Snackbar.make(view, "Empty", 1000);
+            notice.show();
+        }else{
+            MainActivity.list.setName(extras.getInt("key"), name.getText().toString());
+            MainActivity.list.setDescription(extras.getInt("key"), des.getText().toString());
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+    public void addToList(View view) {
         EditText text = findViewById(R.id.taskName);
+        EditText textDescription = findViewById(R.id.editTextTextPersonName2);
         if(text.getText().toString().equals("")){
             Snackbar notice = Snackbar.make(view, "Empty", 1000);
             notice.show();
         }else{
-            MainActivity.list.add(text.getText().toString());
+            MainActivity.list.add(text.getText().toString(), textDescription.getText().toString());
             startActivity(new Intent(this, MainActivity.class));
         }
     }
